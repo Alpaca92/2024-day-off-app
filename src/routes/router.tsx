c85f68172b442a5487c7c93955ceb58c4e90d563
Alpaca92 <zxcvbnm5288@gmail.com>
2024-10-07 15:40:42 +0900

    feat: home init

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,9 +1,14 @@
 import { lazy } from 'react';
 import { RouteObject } from 'react-router';
 
+const Home = lazy(() => import('@pages/Home/Home'));
 const Login = lazy(() => import('@pages/Login/Login'));
 
 const Router: RouteObject[] = [
+  {
+    path: '/',
+    element: <Home />,
+  },
   {
     path: '/login',
     element: <Login />,
