78dc4dd64225804464baa2c4022d141cb1e0e4cc
Alpaca92 <zxcvbnm5288@gmail.com>
2024-10-16 13:05:25 +0900

    fix: setup the story path alias

---
+++ .storybook/preview.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import type { Preview } from '@storybook/react';
+import { ThemeProvider } from '@providers/ThemeProvider';
+import { darkTheme, lightTheme } from '@styles/theme.css';
+
+const preview: Preview = {
+  globalTypes: {
+    theme: {
+      name: 'theme',
+      description: 'change theme',
+      defaultValue: false,
+      toolbar: {
+        title: 'theme',
+        icon: 'wrench',
+        items: [
+          { title: 'dark', value: true, right: 'dark mode' },
+          { title: 'light', value: false, right: 'light mode' },
+        ],
+      },
+    },
+  },
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+};
+
+export const decorators = [
+  (Story, context) => {
+    const { theme } = context.globals;
+
+    return (
+      <ThemeProvider>
+        <div className={`${theme ? darkTheme : lightTheme}`}>
+          <Story />
+        </div>
+      </ThemeProvider>
+    );
+  },
+];
+
+export default preview;
