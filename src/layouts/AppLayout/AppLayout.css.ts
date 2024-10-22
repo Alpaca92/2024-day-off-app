edc8fc3d2d469de32a0f60467c8af716ef6e790d
Alpaca92 <zxcvbnm5288@gmail.com>
2024-10-22 17:07:31 +0900

    styles: set max width 500px on app layout

--- src/layouts/AppLayout/AppLayout.css.ts
+++ src/layouts/AppLayout/AppLayout.css.ts
@@ -2,6 +2,7 @@ import { style } from '@vanilla-extract/css';
 
 export const container = style({
   width: '100%',
+  maxWidth: '500px',
   height: '100vh',
   display: 'grid',
   gridTemplateRows: '14fr 1fr',
--- src/layouts/AppLayout/AppLayout.tsx
+++ src/layouts/AppLayout/AppLayout.tsx
@@ -1,12 +1,42 @@
 import { Outlet } from 'react-router';
-import * as styles from './AppLayout.css';
 import { NavigationBar } from '@components/index';
+import * as styles from './AppLayout.css';
+import { Icons } from '@asset/index';
+import { NavigationBarProps } from '@components/NavigationBar/NavigationBar';
+
+const items: NavigationBarProps['items'] = [
+  {
+    src: Icons.Home,
+    alt: 'Home',
+    title: 'Home',
+  },
+  {
+    src: Icons.Home,
+    alt: 'Home',
+    title: 'Home',
+  },
+  {
+    src: Icons.Home,
+    alt: 'Home',
+    title: 'Home',
+  },
+  {
+    src: Icons.Home,
+    alt: 'Home',
+    title: 'Home',
+  },
+  {
+    src: Icons.Home,
+    alt: 'Home',
+    title: 'Home',
+  },
+];
 
 const AppLayout = () => {
   return (
     <main className={styles.container}>
       <Outlet />
-      <NavigationBar />
+      <NavigationBar items={items} />
     </main>
   );
 };
