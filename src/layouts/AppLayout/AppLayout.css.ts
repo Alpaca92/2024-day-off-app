16bd1a09a295ed83157c42ccb02e4b948f1746c5
Alpaca92 <zxcvbnm5288@gmail.com>
2024-10-22 16:41:00 +0900

    styles: app은 일반적으로 navbar와 13~14:1 비율

--- src/layouts/AppLayout/AppLayout.css.ts
+++ src/layouts/AppLayout/AppLayout.css.ts
@@ -1,6 +1,8 @@
-import { vars } from '@styles/theme.css';
 import { style } from '@vanilla-extract/css';
 
 export const container = style({
   width: '100%',
+  height: '100vh',
+  display: 'grid',
+  gridTemplateRows: '14fr 1fr',
 });
--- src/layouts/AppLayout/AppLayout.tsx
+++ src/layouts/AppLayout/AppLayout.tsx
@@ -1,11 +1,12 @@
 import { Outlet } from 'react-router';
 import * as styles from './AppLayout.css';
+import { NavigationBar } from '@components/index';
 
 const AppLayout = () => {
   return (
     <main className={styles.container}>
       <Outlet />
-      {/* <Nav /> */}
+      <NavigationBar />
     </main>
   );
 };
