de18b87b712fe3314a502032a21e8e56c2bae3db
Alpaca92 <zxcvbnm5288@gmail.com>
2024-10-22 17:33:50 +0900

    chore: font 적용 준비

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,8 +1,18 @@
 // NOTE fonts
+import NoonnuBasicGothicOTF from './fonts/NoonnuBasicGothicRegular.otf';
+import NoonnuBasicGothicTTF from './fonts/NoonnuBasicGothicRegular.ttf';
+
 // NOTE icons
 import Home from './images/home.svg';
 import Profile from './images/profile.svg';
 import Request from './images/request.svg';
+
+export const fonts = {
+  NoonnuBasicGothic: {
+    otf: NoonnuBasicGothicOTF,
+    ttf: NoonnuBasicGothicTTF,
+  },
+};
 
 export const Icons = {
   Home,
