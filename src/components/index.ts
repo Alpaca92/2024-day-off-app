eedbd467c588494a0b1f381d45e71ac9a44dbd44
Alpaca92 <zxcvbnm5288@gmail.com>
2024-10-23 16:30:51 +0900

    📦 chore: Add comment

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,5 @@
 import Button from './Button/Button';
-import Header from './Header/Header';
+import Header from './Header/Header'; // NOTE Header component is not used in this project
 import Icon from './Icon/Icon';
 import Input from './Input/Input';
 import NavigationBar from './NavigationBar/NavigationBar';
