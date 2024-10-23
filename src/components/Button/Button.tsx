e5909aa0e88073d72095905ebcbc60e9e99a5c89
Alpaca92 <zxcvbnm5288@gmail.com>
2024-10-23 14:28:44 +0900

    Allow undefined props in button component (use "...rest")

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -5,8 +5,12 @@ import clsx from 'clsx';
 // eslint-disable-next-line @typescript-eslint/no-empty-object-type
 interface ButtonProps extends ComponentPropsWithoutRef<'button'> {}
 
-const Button = ({ children, className, variant, fontSize }: ButtonProps & ButtonRecipeProps) => {
-  return <button className={clsx(button({ variant, fontSize }), className)}>{children}</button>;
+const Button = ({ children, className, variant, fontSize, ...rest }: ButtonProps & ButtonRecipeProps) => {
+  return (
+    <button className={clsx(button({ variant, fontSize }), className)} {...rest}>
+      {children}
+    </button>
+  );
 };
 
 export default Button;
