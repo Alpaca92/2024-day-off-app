abe8179ceef643ce0de2e2211fc1f37f14d810d0
Alpaca92 <zxcvbnm5288@gmail.com>
2024-10-23 10:45:11 +0900

    chore: add icons (management, setting)

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -4,8 +4,10 @@ import NoonnuBasicGothicTTF from './fonts/NoonnuBasicGothicRegular.ttf';
 
 // NOTE icons
 import Home from './images/home.svg';
+import Management from './images/management.svg';
 import Profile from './images/profile.svg';
 import Request from './images/request.svg';
+import Setting from './images/setting.svg';
 
 export const fonts = {
   NoonnuBasicGothic: {
@@ -16,6 +18,8 @@ export const fonts = {
 
 export const Icons = {
   Home,
+  Management,
   Profile,
   Request,
+  Setting,
 };
