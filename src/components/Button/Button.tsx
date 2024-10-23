4f2bb8ed42f0013b602322b529146fb617740f84
Alpaca92 <zxcvbnm5288@gmail.com>
2024-10-23 14:07:59 +0900

    styles: Modify the button to accept a className prop and combine the class names

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,12 @@
 import { ComponentPropsWithoutRef } from 'react';
 import { button, ButtonRecipeProps } from './Button.css';
+import clsx from 'clsx';
 
 // eslint-disable-next-line @typescript-eslint/no-empty-object-type
 interface ButtonProps extends ComponentPropsWithoutRef<'button'> {}
 
-const Button = ({ children, variant, fontSize }: ButtonProps & ButtonRecipeProps) => {
-  return <button className={button({ variant, fontSize })}>{children}</button>;
+const Button = ({ children, className, variant, fontSize }: ButtonProps & ButtonRecipeProps) => {
+  return <button className={clsx(button({ variant, fontSize }), className)}>{children}</button>;
 };
 
 export default Button;
