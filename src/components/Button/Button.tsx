c67724835810a08c8d4d831b42323402e3586f51
Alpaca92 <zxcvbnm5288@gmail.com>
2024-10-24 17:34:56 +0900

    🐛 fix: Prevent default mouse behavior in Button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,13 +1,18 @@
-import { ComponentPropsWithoutRef } from 'react';
+import { ComponentPropsWithoutRef, MouseEvent } from 'react';
 import { button, ButtonRecipeProps } from './Button.css';
 import clsx from 'clsx';
 
 // eslint-disable-next-line @typescript-eslint/no-empty-object-type
 interface ButtonProps extends ComponentPropsWithoutRef<'button'> {}
 
-const Button = ({ children, className, variant, fontSize, ...rest }: ButtonProps & ButtonRecipeProps) => {
+const Button = ({ variant, fontSize, children, className, onClick, ...rest }: ButtonProps & ButtonRecipeProps) => {
+  const _onClick = (event: MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    onClick?.(event);
+  };
+
   return (
-    <button className={clsx(button({ variant, fontSize }), className)} {...rest}>
+    <button className={clsx(button({ variant, fontSize }), className)} onClick={_onClick} {...rest}>
       {children}
     </button>
   );
