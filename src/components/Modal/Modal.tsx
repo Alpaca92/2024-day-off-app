cb771f5defa8040c6230a7ce899a05462f3e383a
acapla <zxcvbnm5288@gmail.com>
2024-10-24 21:42:15 +0900

    🚀 feat: Create modal component

---
+++ src/components/Modal/Modal.tsx
@@ -0,0 +1,18 @@
+import { ReactNode } from 'react';
+import * as styles from './Modal.css';
+
+interface ModalProps {
+  children: ReactNode;
+  title: string;
+}
+
+const Modal = ({ children, title }: ModalProps) => {
+  return (
+    <>
+      <h1 className={styles.title}>{title}</h1>
+      {children}
+    </>
+  );
+};
+
+export default Modal;
--- src/components/index.ts
+++ src/components/index.ts
@@ -2,6 +2,7 @@ import Button from './Button/Button';
 import Header from './Header/Header'; // NOTE Header component is not used in this project
 import Icon from './Icon/Icon';
 import Input from './Input/Input';
+import Modal from './Modal/Modal';
 import NavigationBar from './NavigationBar/NavigationBar';
 
-export { Input, Button, Header, Icon, NavigationBar };
+export { Input, Button, Header, Icon, Modal, NavigationBar };
