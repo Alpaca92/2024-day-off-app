6a1d7d85876f06219e9cd89ae5c9428ae6d3a231
Alpaca92 <zxcvbnm5288@gmail.com>
2024-11-04 07:57:16 +0900

    📦 chore: update eslint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -8,12 +8,12 @@ import importPlugin from 'eslint-plugin-import';
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    extends: [js.configs.recommended, ...tseslint.configs.recommended, importPlugin.flatConfigs.recommended],
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
@@ -23,48 +23,28 @@ export default tseslint.config(
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
       'import/order': [
-        'error',
+        'warn',
         {
-          groups: [
-            'builtin', // Node.js 내장 모듈
-            'external', // npm 패키지
-            'internal', // 프로젝트 내부 모듈
-            ['sibling', 'parent'], // 상대 경로 import
-            'index', // 현재 디렉토리 import
-            'type', // 타입 import
-          ],
-          'newlines-between': 'always', // 그룹 간 빈 줄 추가
-          alphabetize: {
-            order: 'asc', // 알파벳 순 정렬
-            caseInsensitive: true,
-          },
+          'newlines-between': 'always',
+          groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
           pathGroups: [
-            // react 관련 패키지를 최상단에 위치
             {
-              pattern: 'react**',
+              pattern: 'react',
               group: 'external',
               position: 'before',
             },
-            // 그 다음 외부 라이브러리
             {
-              pattern: '{clsx,react-router-dom,react-query}',
+              pattern: 'react*',
               group: 'external',
-              position: 'after',
+              position: 'before',
             },
-            // @로 시작하는 경로
             {
-              pattern: '@/**',
+              pattern:
+                '{@/asset/*, @/components/*, @/constants/*, @/hooks/*, @/layouts/*, @/pages/*, @/providers/*, @/routes/*, @/styles/*}',
               group: 'internal',
-              position: 'after',
-            },
-            // 상대 경로
-            {
-              pattern: './**',
-              group: 'sibling',
-              position: 'after',
+              position: 'before',
             },
           ],
-          pathGroupsExcludedImportTypes: ['react'],
         },
       ],
     },
